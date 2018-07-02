faa60f5343f38100295b9f57c2a1eda54b46d697
ramondcsilva <ramondecerqueirasilva@gmail.com>
2018-07-02 14:39:57 -0300

    Commit Testes atualizados, falta controller.

--- test/model/RankingTest.java
+++ test/model/RankingTest.java
@@ -15,6 +15,28 @@ public class RankingTest {
 
     @Test
     public void testBasic() {
+        assertTrue(r.getPalavras().estaVazia());
+        assertEquals(r.getPalavras().size(), 0);
+
+        Palavra p0 = new Palavra("um");
+        Palavra p1 = new Palavra("dois");
+        Palavra p2 = new Palavra("três");
         
+        r.addPalavras(p0);
+        r.addPalavras(p1);
+        
+        assertFalse(r.getPalavras().estaVazia());
+        assertEquals(r.getPalavras().size(), 2);
+        assertTrue(r.getPalavras().contem(p0));
+        assertTrue(r.getPalavras().contem(p1));
+        assertFalse(r.getPalavras().contem(p2));
+        
+        TreeAVL temp = new TreeAVL();
+        temp.insert(p0);
+        temp.insert(p1);
+        
+        r.setPalavras(temp);
+        assertFalse(temp.estaVazia());
+        assertEquals(temp,r.getPalavras());
     }
 }
